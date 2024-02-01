0f752d08d18f4e9fe5070b527b7c04aa909eb146
L2JE <leojesus.juarez@gmail.com>
2024-02-01 14:47:58 -0300

    Join common setup for tasks in one configureEach call, moved base docker env to directory called 'base' and added scmTest task

---
+++ functional-test/src/test/resources/spock-configs/IncludeScmTestConfig.groovy
@@ -0,0 +1,5 @@
+import org.rundeck.util.annotations.ScmTest
+
+runner {
+    include ScmTest
+}
--- functional-test/src/test/resources/spock-configs/IncludeSeleniumCoreTestsConfig.groovy
+++ functional-test/src/test/resources/spock-configs/IncludeSeleniumCoreTestsConfig.groovy
@@ -1,5 +1,6 @@
+import org.rundeck.util.annotations.ScmTest
 import org.rundeck.util.annotations.SeleniumCoreTest
 
 runner {
-    include SeleniumCoreTest
+    include SeleniumCoreTest, ScmTest
 }
